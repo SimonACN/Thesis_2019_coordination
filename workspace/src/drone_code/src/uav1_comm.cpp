da7fa64f5ce431b43abaadd27c9b1c630ff44910
SimonACN <au522230@post.au.dk>
2020-04-14 03:36:35 +0200

    Update 3D Map merging and visualization

--- workspace/src/drone_code/src/uav1_comm.cpp
+++ workspace/src/drone_code/src/uav1_comm.cpp
@@ -11,6 +11,8 @@
 #include <octomap/octomap.h>
 #include <octomap/OcTreeKey.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <cmath> 
+#define UNKNOWN -1
 
 
 
@@ -25,11 +27,11 @@ nav_msgs::OccupancyGrid mergedGridmap;
 bool visualflag = false;
 bool initFlag = false;
 bool initPM = false;
-
+const float resolution = 0.5;
 
 
 void update3DMap();
-void mergeMap(octomap::OcTree*, octomap::OcTree::leaf_iterator);
+void mergeMap(octomap::OcTree*);
 void reset3DMap();
 void initMergedMap();
 void MergeProjectedMap(nav_msgs::OccupancyGrid);
@@ -39,57 +41,75 @@ void initPRM(nav_msgs::OccupancyGrid);
 void initPRM(nav_msgs::OccupancyGrid initMap){
 	mergedGridmap.info = initMap.info;
 	mergedGridmap.header = initMap.header;
-	mergedGridmap.data = initMap.data;
+	mergedGridmap.data.resize(mergedGridmap.info.height* mergedGridmap.info.width,-1);
+	for(int i = 0; i<initMap.info.width*initMap.info.height-1; i++){
+	mergedGridmap.data[i] = initMap.data[i];
+	}
 	initPM = true;
 }
 
 //Merge projected maps
-//Work in progress
 void MergeProjectedMap(nav_msgs::OccupancyGrid gridmap2){
-	float offsetX, offsetY, offsetXPM1, offsetYPM1 = 0;
-	
+	float offsetWidth = 0, offsetHeight = 0, offsetWidthPM1 = 0, offsetHeightPM1 = 0;
 	nav_msgs::OccupancyGrid tempGridmap;
-	tempGridmap.data = mergedGridmap.data;
+	tempGridmap.info = mergedGridmap.info;
+	tempGridmap.data.resize(tempGridmap.info.height * tempGridmap.info.width,-1);
+	for(int i = 0; i<mergedGridmap.info.width*mergedGridmap.info.height-1; i++){
+	tempGridmap.data[i] = mergedGridmap.data[i];
+
+	}
+
 	//Determine which origin is the lowest
-	
-	//Width
-	if(mergedGridmap.info.origin.position.x < gridmap2.info.origin.position.x){
-		offsetX = ceil(-mergedGridmap.info.origin.position.x); //Offset of grid cells when merging the maps
-	}
-	else if(mergedGridmap.info.origin.position.x > gridmap2.info.origin.position.x){
-		mergedGridmap.info.origin.position.x = gridmap2.info.origin.position.x;
-		mergedGridmap.info.height = mergedGridmap.info.height + ceil(-mergedGridmap.info.origin.position.x); //Extend width by origin change
-		offsetXPM1 = ceil(-mergedGridmap.info.origin.position.x); //Offset of grid cells when merging the maps
-	}
-	
-	//Height
+
+	//Height = origin y-position
 	if(mergedGridmap.info.origin.position.y < gridmap2.info.origin.position.y){
-		offsetY = ceil(-mergedGridmap.info.origin.position.y); //Offset of grid cells when merging the maps
+		offsetHeight = ceil((gridmap2.info.origin.position.y - mergedGridmap.info.origin.position.y)/resolution); //Offset of grid cells when merging the maps
 	}
 	else if(mergedGridmap.info.origin.position.y > gridmap2.info.origin.position.y){
-		mergedGridmap.info.origin.position.y = gridmap2.info.origin.position.x;
-		mergedGridmap.info.height = mergedGridmap.info.height + ceil(-mergedGridmap.info.origin.position.y); //Extend width by origin change
-		offsetYPM1 = ceil(-mergedGridmap.info.origin.position.y); //Offset of grid cells when merging the maps
+		offsetHeightPM1 = ceil((mergedGridmap.info.origin.position.y - gridmap2.info.origin.position.y)/resolution); //Offset of grid cells when merging the maps
+		mergedGridmap.info.origin.position.y = gridmap2.info.origin.position.y;
+		mergedGridmap.info.height = mergedGridmap.info.height + offsetHeightPM1; //Extend height by origin change
+		
+		
 	}
+	
+	//Width = origin x-position
+	if(mergedGridmap.info.origin.position.x < gridmap2.info.origin.position.x){
+		offsetWidth = ceil((gridmap2.info.origin.position.x - mergedGridmap.info.origin.position.x)/resolution); //Offset of grid cells when merging the maps
+	}
+	else if(mergedGridmap.info.origin.position.x > gridmap2.info.origin.position.x){
+		offsetWidthPM1 = ceil((mergedGridmap.info.origin.position.x - gridmap2.info.origin.position.x)/resolution); //Offset of grid cells when merging the maps
+		mergedGridmap.info.origin.position.x = gridmap2.info.origin.position.x;
+		mergedGridmap.info.width = mergedGridmap.info.width + offsetWidthPM1; //Extend width by origin change
+		
+	}
+
 	
 	mergedGridmap.data.clear();
 	mergedGridmap.data.resize(mergedGridmap.info.height* mergedGridmap.info.width,-1);
-	
-	for(int x = 0; x<tempGridmap.info.width; x++){
-		for(int y = 0; y <tempGridmap.info.height;y++){
+
+	//Grid map is like a matrix. Column = height and row = width relative to y-position direction in Rviz.
+	for(int c = 0; c<tempGridmap.info.height; c++){
+		for(int r = 0; r <tempGridmap.info.width;r++){
 			//Copy old map to the new one.
-			if(tempGridmap.data[x*y] != -1)
-				mergedGridmap.data[(x+offsetXPM1)*(y*offsetYPM1)] = tempGridmap.data[x*y];
+			if(tempGridmap.data[(c*tempGridmap.info.width)+r] != UNKNOWN)
+				mergedGridmap.data[((c+offsetHeightPM1)*mergedGridmap.info.width)+(r+offsetWidthPM1)] = tempGridmap.data[(c*tempGridmap.info.width)+r];
+			
+		}		
+	}
+
+
+	
+	
+	for(int c = 0; c<gridmap2.info.height; c++){
+		for(int r = 0; r <gridmap2.info.width;r++){
+			//Add only new data to local map from received map
+			if(mergedGridmap.data[((c+offsetHeight)*mergedGridmap.info.width)+(r+offsetWidth)] == UNKNOWN)
+				mergedGridmap.data[((c+offsetHeight)*mergedGridmap.info.width)+(r+offsetWidth)] = gridmap2.data[(c*gridmap2.info.width)+r];
 		}			
 	}
 	
-	for(int x = 0; x<gridmap2.info.width; x++){
-		for(int y = 0; y <gridmap2.info.height;y++){
-			//Add only new data to local map from received map
-			if(mergedGridmap.data[(x+offsetX)*(y*offsetY)] != -1)
-				mergedGridmap.data[(x+offsetX)*(y*offsetY)] = gridmap2.data[x*y];
-		}			
-	}
+
 
 }
 
@@ -113,51 +133,113 @@ occupied_cells.markers[mergedMap->getTreeDepth()].action = visualization_msgs::M
 }
 
 
-void mergeMap(octomap::OcTree* map1, octomap::OcTree::leaf_iterator it)
+void mergeMap(octomap::OcTree* map1)
 {
-	octomap::point3d point = it.getCoordinate(); //Get coordinates
-	octomap::OcTreeNode *nodeIn1 = mergedMap->search(point); //Search for node
-	if (nodeIn1 != NULL) { 
-		//If local map has occupied/free node, Prioritize local map.
-		if(octree_1->isNodeOccupied(*it)){
-			octomap::OcTreeNode *newNode = mergedMap->updateNode(point, true);
-			newNode->setLogOdds(it->getLogOdds());
+	for (octomap::OcTree::leaf_iterator it = map1->begin_leafs(); it != map1->end_leafs(); ++it) {
+	
+	octomap::OcTreeKey nodeKey = it.getKey(); //Get coordinates
+	octomap::OcTreeNode *node = mergedMap->search(nodeKey); //Search for node
+	//If node exists
+	if (node != NULL) {
+		float logOddsProb = 0;
+		//If both maps show node is occupied
+		if(map1->isNodeOccupied(*it) && mergedMap->isNodeOccupied(node)){
+			
+		
+		//If both maps show node is occupied, prioritize highest probability as that has more data on it. If both are equal, just keep same probability
+		if(it->getLogOdds() > node->getLogOdds())
+			logOddsProb = it->getLogOdds();
+		else if (it->getLogOdds() < node->getLogOdds())
+			logOddsProb = node->getLogOdds();
+		else
+			logOddsProb = node->getLogOdds();
+		
+		mergedMap->setNodeValue(nodeKey,logOddsProb);
+		}
+		//If both maps show node is free
+		else if(!map1->isNodeOccupied(*it) && !mergedMap->isNodeOccupied(node)){
+		
+		//If both maps show node is occupied, prioritize lowest probability as that has more data on it. If both are equal, just keep same probability
+		if(it->getLogOdds() < node->getLogOdds())
+			logOddsProb = it->getLogOdds();
+		else if (it->getLogOdds() >= node->getLogOdds())
+			logOddsProb = node->getLogOdds();
+		
+		mergedMap->setNodeValue(nodeKey,logOddsProb);
+		}
+		//If both maps show have conflicting information, keep prioritize occupancy from received map.
+		else
+		{
+			
+			float logOddsProb;
+			float logOddsDiffMax;
+			float logOddsDiffMin;
+			//If received map has occupied node and current map is free
+			if(map1->isNodeOccupied(node) && !mergedMap->isNodeOccupied(node)){
+				logOddsDiffMin = mergedMap->getClampingThresMinLog() - node->getLogOdds();
+				logOddsDiffMax = map1->getClampingThresMaxLog() - it->getLogOdds();
+				
+				
+				if(logOddsDiffMax < std::abs(logOddsDiffMin))
+					logOddsProb = it->getLogOdds();
+				else if (logOddsDiffMax >= std::abs(logOddsDiffMin))
+					logOddsProb = node->getLogOdds();
+					
+			}
+			//If received map has free node and current map is occupied
+			else if(!map1->isNodeOccupied(node) && mergedMap->isNodeOccupied(node)){
+				logOddsDiffMin = map1->getClampingThresMinLog() - it->getLogOdds();
+				logOddsDiffMax = mergedMap->getClampingThresMaxLog() - node->getLogOdds();
+				
+				if(logOddsDiffMax > std::abs(logOddsDiffMin))
+					logOddsProb = it->getLogOdds();
+				else if (logOddsDiffMax <= std::abs(logOddsDiffMin))
+					logOddsProb = node->getLogOdds();
+			}
+			
+			/*
+			//Prioritize occupancy
+			if(map1->isNodeOccupied(*it) || mergedMap->isNodeOccupied(node)){
+				logOddsProb = std::max(it->getLogOdds(),node->getLogOdds());
+			}
+			mergedMap->setNodeValue(nodeKey,logOddsProb);
+			*/
 		}
 		
-		else{
-			octomap::OcTreeNode *newNode = mergedMap->updateNode(point, false);
-			newNode->setLogOdds(it->getLogOdds());
-			}
+	}
+		//If node doesn't exist, set probability and node key
+	else {
+		mergedMap->setNodeValue(nodeKey, it->getLogOdds());		
 		}
-		//If node doesn't exist, copy probability and node key
-		else{
-			octomap::OcTreeKey nodeKey = map1->coordToKey(point);
-			mergedMap->updateNode(nodeKey, it->getLogOdds());
-
-			}
-			
-			
-}
-
-
-void update3DMap(){
-//Visualize map of merged map
-reset3DMap();
-for (octomap::OcTree::leaf_iterator it = mergedMap->begin_leafs(); it != mergedMap->end_leafs(); ++it){
+		
+	//Visualizer	
 	if(mergedMap->isNodeOccupied(*it)){
 	geometry_msgs::Point cubeCenter;
     cubeCenter.x = it.getX();
     cubeCenter.y = it.getY();
     cubeCenter.z = it.getZ();
-	if(cubeCenter.z > 1){
-    occupied_cells.markers[mergedMap->getTreeDepth()].points.push_back(cubeCenter);
+	if(cubeCenter.z > 1)
+		occupied_cells.markers[mergedMap->getTreeDepth()].points.push_back(cubeCenter);
+	
+	}
+	}
+}
+
+
+void update3DMap(){
+//Visualize map of merged map
+//reset3DMap();
+
+
 	if (occupied_cells.markers[mergedMap->getTreeDepth()].points.size() > 0)
         occupied_cells.markers[mergedMap->getTreeDepth()].action = visualization_msgs::Marker::ADD;
     else
         occupied_cells.markers[mergedMap->getTreeDepth()].action = visualization_msgs::Marker::DELETE;
-	}
-	}
-}
+	
+
+
+	
+
 }
 
 
@@ -169,13 +251,13 @@ if(!initFlag)
 	initMergedMap();
 
 if(visualflag){
-for (octomap::OcTree::leaf_iterator it = octree_1->begin_leafs(); it != octree_1->end_leafs(); ++it) {
-mergeMap(octree_1,it);
-
-}
+mergeMap(octree_1);
 update3DMap();
 }
+
+
 }
+
 
 //Merging map callback
 void MergeMapCallback(const octomap_msgs::Octomap::ConstPtr& msg){
@@ -185,12 +267,12 @@ tree0 = octomap_msgs::msgToMap(*msg);
 octree_0 = dynamic_cast<octomap::OcTree*>(tree0);
 
 if(visualflag && initFlag){
-for (octomap::OcTree::leaf_iterator it = octree_0->begin_leafs(); it != octree_0->end_leafs(); ++it) {
- mergeMap(octree_0,it);
-}
-	 update3DMap();	
+	mergeMap(octree_0);
+	update3DMap();
 }
 }
+
+
 
 
 //Get visualizer configurations
@@ -218,8 +300,8 @@ void MergeProjectedMapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg){
 	receivedGridmap.info = msg->info;
 	receivedGridmap.header = msg->header;
 	receivedGridmap.data = msg->data;
-	//if(initPM)
-		//MergeProjectedMap(receivedGridmap);
+	if(initPM)
+		MergeProjectedMap(receivedGridmap);
 }
 
 
